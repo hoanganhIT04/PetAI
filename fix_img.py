438da909f953d308680e0743433aecf77d8fda0b
ThNam2310 <namkim102@gmail.com>
2026-02-04 16:23:31 +0700

    Sửa tên nature

--- fix_img.py
+++ fix_img.py
@@ -1,10 +1,10 @@
 import os
 import re
 
-FOLDER_PATH = r"A:\NCKH_Web\PetAI\frontend\public\assets\data\data_model_1\non_animal\food"
+FOLDER_PATH = r"A:\NCKH_Web\PetAI\frontend\public\assets\data\data_model_1\non_animal\nature"
 IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png", ".webp", ".bmp")
 
-pattern = re.compile(r"^food_\d+\.(jpg|jpeg|png|webp|bmp)$", re.IGNORECASE)
+pattern = re.compile(r"^nature_\d+\.(jpg|jpeg|png|webp|bmp)$", re.IGNORECASE)
 
 # Lấy các file CHƯA đúng format
 files = [
@@ -17,7 +17,7 @@ files.sort()
 # Tìm index lớn nhất hiện có
 existing_indexes = []
 for f in os.listdir(FOLDER_PATH):
-    m = re.match(r"food_(\d+)\.", f, re.IGNORECASE)
+    m = re.match(r"nature_(\d+)\.", f, re.IGNORECASE)
     if m:
         existing_indexes.append(int(m.group(1)))
 
@@ -25,11 +25,11 @@ start_index = max(existing_indexes) + 1 if existing_indexes else 1
 
 for idx, filename in enumerate(files, start=start_index):
     ext = os.path.splitext(filename)[1]
-    new_name = f"food_{idx:03d}{ext}"
+    new_name = f"nature_{idx:03d}{ext}"
 
     old_path = os.path.join(FOLDER_PATH, filename)
     new_path = os.path.join(FOLDER_PATH, new_name)
 
     os.rename(old_path, new_path)
 
-print(f"Đã đổi tên {len(files)} ảnh food (an toàn, không ghi đè).")
+print(f"Đã đổi tên {len(files)} ảnh nature (an toàn, không ghi đè).")
